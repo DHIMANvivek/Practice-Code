bb51759101974a1a15ba39daffc6e6f04f32d06d
DHIMANvivek <googlydhiman.4236@gmail.com>
2021-09-19 19:21:06 +0530

    Literal In Java

--- com.interview/src/main/java/com/interview/Literal_Magic.java
+++ com.interview/src/main/java/com/interview/Literal_Magic.java
@@ -1,28 +1,20 @@
 package com.interview;
 
-class Literal_Magic{
-	
+class Literal_Magic {
 
-
-public static void main(String arg[]) {
-
-	
+	public static void main(String arg[]) {
 		
-	
-	
+		//in this all the value that will going to be an output
+		// same for all , default out due to literal is 10
+
 		byte b1 = 10;
-		byte b2 = 0b10;
-		byte b3 = 0XA;
-		byte b4 = 012;
-		// TODO Auto-generated method stub
-		System.out.println("as we take b1 as 10 "+b1);
-		System.out.println("as we take b2 as 0b10 "+b2);
-		System.out.println("as we take b3 as 0XA "+b3);
-		System.out.println("as we take b4 as 012 "+b4);
-	
-	
-	
-	
-}
+		byte b2 = 0XA;
+		byte b3 = 012;
+
+		System.out.println("as we take b1 as 10 is " + b1);
+		System.out.println("as we take b3 as 0XA is " + b2);
+		System.out.println("as we take b4 as 012 is " + b3);
+
+	}
 
 }
