d64a73df0c15c685334473c961b9e6bacdf805ea
DHIMANvivek <googlydhiman.4236@gmail.com>
2021-09-19 19:18:43 +0530

    Literal in java magic

---
+++ com.interview/src/main/java/com/interview/Literal_Magic.java
@@ -0,0 +1,28 @@
+package com.interview;
+
+class Literal_Magic{
+	
+
+
+public static void main(String arg[]) {
+
+	
+		
+	
+	
+		byte b1 = 10;
+		byte b2 = 0b10;
+		byte b3 = 0XA;
+		byte b4 = 012;
+		// TODO Auto-generated method stub
+		System.out.println("as we take b1 as 10 "+b1);
+		System.out.println("as we take b2 as 0b10 "+b2);
+		System.out.println("as we take b3 as 0XA "+b3);
+		System.out.println("as we take b4 as 012 "+b4);
+	
+	
+	
+	
+}
+
+}
