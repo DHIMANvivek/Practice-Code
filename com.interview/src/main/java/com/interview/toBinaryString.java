37a88cbe2d41dee44d45f545419ab96d8979f7ed
DHIMANvivek <googlydhiman.4236@gmail.com>
2021-09-19 22:08:08 +0530

    unicode

--- com.interview/src/main/java/com/interview/toBinaryString.java
+++ com.interview/src/main/java/com/interview/toBinaryString.java
@@ -5,13 +5,15 @@ public class toBinaryString {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int x = 5;
-		int y  = -5;
-		
-		// this will give a binary number with one's complement 
-		System.out.println("+5 in 1's complement is "+Integer.toBinaryString(x));
+		int y = -5;
+
+		// this will give a binary number with one's complement
+		System.out.println("+5 in 1's complement is " + Integer.toBinaryString(x));
 
 		// this will give two's complement of a negative number
-		System.out.println("-5 in 2's complement is "+Integer.toBinaryString(y));
+		System.out.println("-5 in 2's complement is " + Integer.toBinaryString(y));
+
+		// remember :- 2's complement always ends with 011
 	}
 
 }
---
+++ com.interview/src/main/java/com/interview/uniCodes.java
@@ -0,0 +1,14 @@
+package com.interview;
+
+public class uniCodes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// to use unicode use 0x <- x is the character optional take any other character also
+		char x = (char) 0x104D;
+		System.out.println(x);
+
+	}
+
+}
