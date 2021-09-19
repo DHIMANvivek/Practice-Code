fd26290a75e808150c05414fa85c82141fd3294b
DHIMANvivek <googlydhiman.4236@gmail.com>
2021-09-19 22:17:56 +0530

    unicode printing of character's + number's

--- com.interview/src/main/java/com/interview/uniCodes.java
+++ com.interview/src/main/java/com/interview/uniCodes.java
@@ -4,10 +4,16 @@ public class uniCodes {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		// to use unicode use 0x <- x is the character optional take any other character also
+
+		// to use unicode use 0x <- x is the character optional take any other character
+		// also
 		char x = (char) 0x104D;
 		System.out.println(x);
+		
+		// this will print the numbers from 0 to 68
+		for(int c = 0x0000 ; c<0x0045 ; c++) {
+			System.out.println("Number here we choose is "+c+"\n");
+		}
 
 	}
 
