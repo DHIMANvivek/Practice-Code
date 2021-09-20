ccb4098c8799fe9e43979fd8e5cc46896bf3797a
DHIMANvivek <googlydhiman.4236@gmail.com>
2021-09-20 20:44:33 +0530

    big oh

---
+++ com.interview/src/main/java/com/interview/BigO_Problems/oBracket1.java
@@ -0,0 +1,22 @@
+package com.interview.BigO_Problems;
+
+public class oBracket1 {
+
+	static int x = 2;
+	static int y = 5;
+
+	public static void main(String[] args) {
+
+		// Big O describes the worst-case scenario i.e the longest amount of time taken
+		// to execute the program.
+
+		// O(1) – Constant
+
+		// Note:O(1) is best Time Complexity method.
+
+		int f = x + y; // O(1) complexity
+		System.out.println(f);
+
+	}
+
+}
